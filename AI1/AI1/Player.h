9e7d57706b6e1e23f2e6e67214206237aa83baf3
Sean Kearns <c00194323@gmail.com>
2017-12-01 11:59:03 +0000

    Movement manual merge

--- AI1/AI1/Player.h
+++ AI1/AI1/Player.h
@@ -1,17 +1,30 @@
 #pragma once
 #include <SFML\Graphics.hpp>
+#include "InputManager.h"
+
 class Player
 {
 public:
-	Player();
+	Player(InputManager *i);
 	~Player();
-	void Render();
+	void Render(sf::RenderWindow &w);
 	void Update();
-
+	sf::View GetView();
 
 private:
 	//Player Variables
-	sf::Texture m_PlayerTextur;
+	sf::Texture m_PlayerTexture;
 	sf::Sprite m_Player;
+
+	sf::Vector2f m_Velocity;
+
+	InputManager *m_Input;
+
+	sf::View camera;
+	
+	float m_Speed;
+	float m_Orientation;
+
+	sf::Vector2f setVelocity(float &currentOrientation);
 };
 
--- AI1/AI1/main.cpp
+++ AI1/AI1/main.cpp
@@ -2,6 +2,7 @@
 #include <SFML\Graphics.hpp>
 #include "InputManager.h"
 #include "Menu.h"
+#include "Player.h"
 
 using namespace std;
 
@@ -11,21 +12,27 @@ static const int windowHeight = 768;
 
 void main()
 {
-	sf::RenderWindow window(sf::VideoMode(1024, 768), "AI");
+	sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "AI");
 	window.setFramerateLimit(60);
 
 	InputManager *input = new InputManager(event);
 	Menu m_Menu(windowWidth, windowHeight, input);
+	Player p(input);
 
 	while (window.isOpen())
 	{
 		//check input
 		input->CheckInput(window);
+
+		//Update
+		p.Update();
+		window.setView(p.GetView());
 		m_Menu.Update();
 
 		//draw
 		window.clear();
 		m_Menu.draw(window);
+		p.Render(window);
 		window.display();
 	}
 }
